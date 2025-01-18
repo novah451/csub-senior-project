008d836a2d489825e6463d2e026d41b937865e9e
Jose Zamora <jzamora0451@gmail.com>
2025-01-17 23:56:02 -0800

    run file should now work automatically on either linux or mac -> no need to manually change the commands when switching from os to os

--- run.sh
+++ run.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-year=$(date -v-1y +%Y)
-month=$(date +%m)
-day=$(date +%d)
-yesterday=$(date -v-1d +%d)
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux/GNU
+    year=$(date -d "1 year ago" +%Y)
+    month=$(date +%m)
+    day=$(date +%d)
+    yesterday=$(date -d "1 day ago" +%d)
 
-hour=$(date +"%H")
-p_hour=$(date -v-6H +%H)
+    hour=$(date +"%H")
+    p_hour=$(date -d "6 hours ago" +%H)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    year=$(date -v-1y +%Y)
+    month=$(date +%m)
+    day=$(date +%d)
+    yesterday=$(date -v-1d +%d)
+
+    hour=$(date +"%H")
+    p_hour=$(date -v-6H +%H)
+fi
 
 echo $year $month $day $yesterday $hour $p_hour
 
-if [ "$hour" -eq 12 ]; then
+if [[ "$hour" -eq 12 ]]; then
     # cleanup some stuff first
     rm weather_data/aurora/*.nc
     rm predictions/aurora/*_predictions_$year-$month-$yesterday.csv
